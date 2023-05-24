2e820cbecf39a98adf258653d1604b8da7f673ad
igor dinic <dinicigor84@gmail.com>
2023-05-24 20:22:17 +0200

    4-bookmarks part completed

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -15,7 +15,7 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @bookmark = Bookmark.find[params[:id]]
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy!
     redirect_to list_path(@bookmark.list), status: :see_other
   end
